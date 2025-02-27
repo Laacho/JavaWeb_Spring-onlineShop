e0c6c5c821e8542491ffd8e1553dff658e131feb
Lachezar Radushev <lachezarradushev@gmail.com>
2025-02-27 18:01:25 +0200

    implemented login and register logic

---
+++ src/main/java/app/web/dto/RegisterRequest.java
@@ -0,0 +1,18 @@
+package app.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Username cant be empty")
+    @Size(min = 3, max = 30,message = "Username must be between 3 and 30 characters!")
+    private String username;
+    @NotBlank(message = "Password cant be empty")
+    @Size(min = 3, max = 30,message = "Password must be between 3 and 30 characters!")
+    private String password;
+    @NotBlank(message = "Address cant be empty")
+    private String address;
+}
